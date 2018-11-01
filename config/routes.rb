7b26b48d0b99d60a163216a88130beecadfa8988
Noulmon <noulmon.noufal@navigantbpm.com>
2018-11-01 18:45:26 +0530

    add edit update

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'about', to: 'home#about'
 
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
