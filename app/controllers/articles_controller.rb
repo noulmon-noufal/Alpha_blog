e4bab3496a4b6750f78d4d459c76a5eed0f6d19d
Noulmon <noulmon.noufal@navigantbpm.com>
2018-11-05 14:20:00 +0530

    Added admin permissions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -57,7 +57,7 @@ class ArticlesController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @article.user
+    if current_user != @article.user && !current_user.admin?
       flash[:danger] = "You cant only edit/delete your articles"
       redirect_to root_path
     end
